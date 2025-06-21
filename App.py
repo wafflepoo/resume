1933453fcc7ddb7292a389db9e1c303d73ee55ce
missaoui <alissamissaoui200@gmail.com>
2025-06-21 22:24:39 +0200

    test

--- App.py
+++ App.py
@@ -64,11 +64,11 @@ nltk.download('punkt')
 
 import spacy
 try:
-    nlp = spacy.load('en_core_web_sm')
+    nlp = spacy.load("en_core_web_sm")
 except:
-    from spacy.cli import download
-    download('en_core_web_sm')
-    nlp = spacy.load('en_core_web_sm')
+    import en_core_web_sm
+    nlp = en_core_web_sm.load()
+
 
 
 
