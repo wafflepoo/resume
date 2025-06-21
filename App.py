8a01a4dbae35cb303dde480980484eef6f80bb2e
missaoui <alissamissaoui200@gmail.com>
2025-06-21 22:05:23 +0200

    ajout de nltk_data

--- App.py
+++ App.py
@@ -46,7 +46,7 @@ from Courses import ds_course,web_course,android_course,ios_course,uiux_course,r
 import pafy #for uploading youtube videos
 import plotly.express as px #to create visualisations at the admin session
 import nltk
-nltk.download('stopwords')
+nltk.data.path.append('./nltk_data')
 
 
 
