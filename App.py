807cbb91b1a317b4ac1c78cdcc84eff72f8a1e0a
missaoui <alissamissaoui200@gmail.com>
2025-06-21 22:12:47 +0200

    Fix NLTK stopwords loading on Streamlit Cloud

--- App.py
+++ App.py
@@ -26,6 +26,12 @@
 
 # 4. CONTINUE WITH THE FOLLOWING CODE...
 
+import nltk
+
+# Télécharger les ressources nécessaires AVANT d’importer ResumeParser
+nltk.download('stopwords')
+nltk.download('punkt')
+
 import streamlit as st
 import pandas as pd
 import base64,random
@@ -45,7 +51,7 @@ from pymongo import MongoClient
 from Courses import ds_course,web_course,android_course,ios_course,uiux_course,resume_videos,interview_videos
 import pafy #for uploading youtube videos
 import plotly.express as px #to create visualisations at the admin session
-import nltk
+
 
 
 
